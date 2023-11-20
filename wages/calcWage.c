f63c65fb0f50f3460977cd744507ba390be4b0c0
Abuemkeze Chu <abuemkeze@gmail.com>
2023-11-21 00:44:38 +0100

    program to calculate wages

--- wages/calcWage.c
+++ wages/calcWage.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+int hours, rate, wages;
+
 int main(int argc, char const *argv[])
 {
-  printf("Program to calculate someones wages");
+  printf("------Program to calculate someones wages------");
+
+  printf("\nPlease enter the number of hours worked you bollo today: ");
+  scanf("%d", &hours);
+  printf("Enter your rate per hour: \n");
+  scanf("%d", &rate);
+
+  wages = rate * hours;
+  printf("Your wages for today (based of the number of hours worked) is: %d\n", wages);
+
   return 0;
 }
