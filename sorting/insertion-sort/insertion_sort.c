e76dac6b5d70eddd04d0afc6a5688002784faf43
Abuemkeze Chu <abuemkeze@gmail.com>
2024-01-25 18:59:13 +0100

    initial commit: insertion sort

---
+++ sorting/insertion-sort/insertion_sort.c
@@ -0,0 +1,13 @@
+/**
+ * implementation of insertion sort algorithm
+ *
+ * input: array of n integers
+ * output: sort in increasing order.
+ */
+
+#include <stdio.h>
+int main()
+{
+
+  return (0);
+}
