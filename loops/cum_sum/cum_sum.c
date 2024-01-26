940f94186d7360a4bbd7402d647f216c65bd0d1f
Abuemkeze Chu <abuemkeze@gmail.com>
2024-01-26 23:29:05 +0100

    another interesting for loop

--- loops/cum_sum/cum_sum.c
+++ loops/cum_sum/cum_sum.c
@@ -3,9 +3,13 @@ int main()
 {
   int cum_sum = 0;
   typedef int counter;
-  for (counter i = 1; i <= 100; i++)
+  int number;
+
+  printf("Enter a number: ");
+  scanf("%d", &number);
+  for (counter i = 1; i <= number; i++)
   {
     cum_sum = cum_sum + i;
   }
-  printf(" cumulative sum = %d\n", cum_sum);
+  printf("The cumulative sum from 1 to %d = %d\n",number, cum_sum);
 }
--- type_casting/type_casting.c
+++ type_casting/type_casting.c
@@ -1,16 +1,26 @@
 /**
  * convert strings into numerics using inbuilt c libraries atoi and atof
-*/
+ */
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main()
+{
   char int_string[] = "1234";
   char float_string[] = "320.5";
   int my_int = atoi(int_string);
   double my_double = atof(float_string);
 
-  printf("int_string = %s, float_string = %s\n", int_string,float_string);
-  printf("my_int = %d, my_double = %.1f\n", my_int,my_double);
+  printf("int_string = %s, float_string = %s\n", int_string, float_string);
+  printf("my_int = %d, my_double = %.1f\n", my_int, my_double);
+
+  int a = 2;
+  double b = 3.14;
+  char myString1[64], myString2[64];
+  sprintf(myString1, "%d", a);
+  sprintf(myString2, "%.2f", b);
+  printf("a=%d, b=%.2f\n", a, b);
+  printf("myString1=%s, myString2=%s\n", myString1, myString2);
+  return 0;
 }
