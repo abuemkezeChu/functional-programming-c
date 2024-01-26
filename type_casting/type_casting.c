87e4ef9db724367c3b830de7802e6a3a1b2fb950
Abuemkeze Chu <abuemkeze@gmail.com>
2024-01-26 17:54:49 +0100

    convert strings to numerics

--- type_casting/type_casting.c
+++ type_casting/type_casting.c
@@ -6,5 +6,11 @@
 #include<stdlib.h>
 
 int main(){
+  char int_string[] = "1234";
+  char float_string[] = "320.5";
+  int my_int = atoi(int_string);
+  double my_double = atof(float_string);
 
+  printf("int_string = %s, float_string = %s\n", int_string,float_string);
+  printf("my_int = %d, my_double = %.1f\n", my_int,my_double);
 }
