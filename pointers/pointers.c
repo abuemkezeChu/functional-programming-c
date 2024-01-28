31b32f05f8ecbbd26b437adf8e2f916c1a50c035
Abuemkeze Chu <abuemkeze@gmail.com>
2024-01-28 03:37:23 +0100

    dereferncing pointers

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -9,8 +9,11 @@ int main()
   printf("value of pAge: %p\n", pAge);
 
   // display the value stored at the address stored in pAge
-  printf("value of age: %d\n",age);
-  printf("value stored at address stored in pAge: %d\n",*pAge);
+  printf("value of age: %d\n", age);
+  printf("value stored at address stored in pAge: %d\n", *pAge);
+  // using dereferencing, change the value stored at age
+  *pAge = 18;
+  printf("New age : %d\n", age);
 
   return 0;
 }
